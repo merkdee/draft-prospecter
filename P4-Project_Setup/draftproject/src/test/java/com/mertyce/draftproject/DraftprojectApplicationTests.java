197a24bb1ec76a621192be3c679f476b39452e01
Mertyce <mdoleman15@gmail.com>
2019-12-01 10:31:05 -0500

    added project setup

---
+++ P4-Project_Setup/draftproject/src/test/java/com/mertyce/draftproject/DraftprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mertyce.draftproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DraftprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
